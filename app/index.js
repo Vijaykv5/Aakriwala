a4b3b7ce0377317866fc558f844896c7c56abbd6
Twentyquid <negativeradiations@gmail.com>
2023-08-31 09:48:42 +0530

    initialized the app. using expo router

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text style={{ fontSize: 24 }}>Home page</Text>
+    </View>
+  );
+}
